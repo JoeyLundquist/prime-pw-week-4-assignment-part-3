319f1e9c9231eb139613e6fecef30497c0f0c1b5
Joey Lundquist <joeylundquist91@gmail.com>
2022-04-18 18:02:22 -0500

    got my base addItem functionality

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,17 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item){
+   basket.push(item);
+   return true;
+}
+console.log(addItem('Apple'));
+console.log(addItem('Banana'));
+console.log(addItem('Orange'));
+console.log(addItem('Pear'));
+console.log(addItem('Plum'));
+console.log(basket);
+
