adcca58765af094acaf5b9ab202a4bc4abc34da0
Joey Lundquist <joeylundquist91@gmail.com>
2022-04-20 15:50:34 -0500

    fixed line 34, ensuring the scope stays in the funciton

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -31,7 +31,7 @@ function removeItem(item){
    }
    
    function listItems(array){
-    for(i=0; i<=array.length-1; i++){
+    for(let i=0; i<=array.length-1; i++){
         console.log(array[i]);
     }
 }
